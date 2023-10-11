9c51a625d264292e3eb14eb631fe47e093cd8260
Ledmington <ledmington.dev@gmail.com>
2023-10-11 23:53:18 +0200

    Added AST nodes

---
+++ app/src/main/java/com/ledmington/ast/OrNode.java
@@ -0,0 +1,34 @@
+/*
+* circuit-finder - A search algorithm to find optimal logic circuits.
+* Copyright (C) 2023-2023 Filippo Barbari <filippo.barbari@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.ledmington.ast;
+
+import java.util.List;
+import java.util.Objects;
+
+public final class OrNode extends Node {
+
+    private final List<Node> nodes;
+
+    public OrNode(final List<Node> nodes) {
+        this.nodes = Objects.requireNonNull(nodes);
+        if (nodes.size() < 2) {
+            throw new IllegalArgumentException(
+                    String.format("Invalid list of nodes: should have had >=2 elements but had %,d", nodes.size()));
+        }
+    }
+}
---
+++ app/src/test/java/com/ledmington/TestNodes.java
@@ -0,0 +1,65 @@
+/*
+* circuit-finder - A search algorithm to find optimal logic circuits.
+* Copyright (C) 2023-2023 Filippo Barbari <filippo.barbari@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.ledmington;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import com.ledmington.ast.AndNode;
+import com.ledmington.ast.BracketsNode;
+import com.ledmington.ast.NotNode;
+import com.ledmington.ast.OrNode;
+import com.ledmington.ast.VariableNode;
+import com.ledmington.ast.ZeroNode;
+
+import org.junit.jupiter.api.Test;
+
+public class TestNodes {
+
+    @Test
+    public void invalidBrackets() {
+        assertThrows(NullPointerException.class, () -> new BracketsNode(null));
+    }
+
+    @Test
+    public void invalidNot() {
+        assertThrows(NullPointerException.class, () -> new NotNode(null));
+    }
+
+    @Test
+    public void invalidAnd() {
+        assertThrows(NullPointerException.class, () -> new AndNode(null));
+        assertThrows(IllegalArgumentException.class, () -> new AndNode(List.of()));
+        assertThrows(IllegalArgumentException.class, () -> new AndNode(List.of(new ZeroNode())));
+    }
+
+    @Test
+    public void invalidOr() {
+        assertThrows(NullPointerException.class, () -> new OrNode(null));
+        assertThrows(IllegalArgumentException.class, () -> new OrNode(List.of()));
+        assertThrows(IllegalArgumentException.class, () -> new OrNode(List.of(new ZeroNode())));
+    }
+
+    @Test
+    public void invalidVariable() {
+        assertThrows(NullPointerException.class, () -> new VariableNode(null));
+        assertThrows(IllegalArgumentException.class, () -> new VariableNode(""));
+        assertThrows(IllegalArgumentException.class, () -> new VariableNode(" "));
+    }
+}
