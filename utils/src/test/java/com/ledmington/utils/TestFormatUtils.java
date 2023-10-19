266f4a060ab1eeba5ca38d14f99a04b6a42d9584
Ledmington <ledmington.dev@gmail.com>
2023-10-19 23:05:17 +0200

    Split core parts of the library into separate modules opt and utils

--- utils/src/test/java/com/ledmington/utils/TestFormatUtils.java
+++ utils/src/test/java/com/ledmington/utils/TestFormatUtils.java
@@ -15,13 +15,11 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-package com.ledmington;
+package com.ledmington.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigInteger;
-
-import com.ledmington.utils.FormatUtils;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
